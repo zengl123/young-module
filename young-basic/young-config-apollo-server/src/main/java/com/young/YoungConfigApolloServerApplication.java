521da69a22993bcc0f938bbd6b6c18958d0f400f
zengling <17363645521@163.com>
2019-05-20 07:28:52 +0800

    携程分布式配置中心

--- young-basic/young-config-apollo-server/src/main/java/com/young/YoungConfigApolloServerApplication.java
+++ young-basic/young-config-apollo-server/src/main/java/com/young/YoungConfigApolloServerApplication.java
@@ -3,6 +3,17 @@ package com.young;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ *Copyright (C), 2018-${YEAR}, 湖南金峰信息科技有限公司
+ * @Description:
+ * @Author: ZENGLING
+ * @钉钉: 17363645521
+ * @CreateDate: 2019/5/16 10:22
+ * @UpdateUser:
+ * @UpdateDate: 2019/5/16 10:22
+ * @UpdateRemark:
+ * @Version:
+ */
 @SpringBootApplication
 public class YoungConfigApolloServerApplication {
 
